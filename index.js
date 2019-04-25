b577acfec712a0bc9910fdbb1256daab9f9c83cf
Varuni Sinha <varuni86@gmail.com>
2019-04-25 11:56:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,14 +7,20 @@
 
 
 function theBeatlesPlay(musicians, instruments) {
+  //create a variable with an empty array.
   var array = [];
+  //Use a for loop, which iterates over the array. of musicians.
+  //The loop should make the same sentence for every member of the musicians array.
   for (var i = 0; i < musicians.length; i++) {
+    //Add this string to the empty array you created.
   	array.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return array;
 }
 
-// + Create a function `johnLennonFacts`. This function will accept one argument, an array of facts about John Lennon (note that it might not be exactly the following facts):
+// + Create a function `johnLennonFacts`.
+//This function will accept one argument, an array of facts about John Lennon
+//(note that it might not be exactly the following facts):
 
 // ```js
 // const facts = [
@@ -25,13 +31,20 @@ function theBeatlesPlay(musicians, instruments) {
 // ];
 // ```
 
-// Use a while loop to loop over the facts array and add `"!!!"` to the end of every fact. The function should return an array of strings with exclamation points.
+// Use a while loop to loop over the facts array and add `"!!!"` to the end of every fact.
+//The function should return an array of strings with exclamation points.
 
 
+// + Create a function `johnLennonFacts`.
+//This function will accept one argument, an array of facts about John Lennon
 function johnLennonFacts(facts) {
+  //create an empty array to hold final results
   var results = [];
   var count = 0;
+  //while(condition){run this code over and over again until condition does not hold true}
   while (count < facts.length) {
+    //add !!! to each element of the facts array
+    //push these new elements to the empty results array
     results.push(facts[count] + "!!!");
     count++;
   }
@@ -49,10 +62,20 @@ function johnLennonFacts(facts) {
 // return newFacts;
 // }
 
-// + Create a function `iLoveTheBeatles` which accepts a number as a parameter. The body of the function should create a variable that stores an empty array. Then, implement a do-while loop inside the function that adds `"I love the Beatles!"` to the empty array. Then the loop should increment the number passed in as a parameter. The condition of the loop should check to see that the parameter number is less than `15`. The function should return the array with the strings `"I love the Beatles!"`.
+// + Create a function `iLoveTheBeatles` which accepts a number as a parameter.
+//The body of the function should create a variable that stores an empty array.
+//Then, implement a do-while loop inside the function that adds `"I love the Beatles!"` to the empty array.
+//Then the loop should increment the number passed in as a parameter.
+//The condition of the loop should check to see that the parameter number is less than `15`.
+//The function should return the array with the strings `"I love the Beatles!"`.
 
+// + Create a function `iLoveTheBeatles` which accepts a number as a parameter.
 function iLoveTheBeatles(num) {
+  //The body of the function should create a variable that stores an empty array.
   var array = [];
+  //Then, implement a do-while loop inside the function that adds `"I love the Beatles!"` to the empty array.
+
+  //do{run this code at least once then check condition} while(condition)
   do {
     array.push("I love the Beatles!")
     num++;
